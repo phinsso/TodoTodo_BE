fd587191397bd4c92840d0b85a15c8a5d70f262b
phinsso <naso01247@gmail.com>
2024-04-02 19:27:24 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/prj/todo/todotodo/TodoTodoApplication.java
@@ -0,0 +1,13 @@
+package prj.todo.todotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prj/todo/todotodo/TodoTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.todo.todotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
