a40313badf994f624b78cdba26cdd126d899ff18
phinsso <naso01247@gmail.com>
2024-05-30 13:43:38 +0900

    #6 Category에 List<Todo> 타입의 필드 생성 (양방향 매핑)

--- src/main/java/prj/todo/todotodo/entity/Category.java
+++ src/main/java/prj/todo/todotodo/entity/Category.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @NoArgsConstructor
 @Getter
@@ -21,6 +23,11 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name; // 카테고리 이름 (종류)
+
+    // 관계의 주인: Todo 엔티티의 category 필드
+    // Category를 삭제할 때 이와 연관된 Todo도 함께 삭제
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Todo> todos;
 
     @Builder
     public Category(Member member, String name) {
